24c676cb57cfc278c4278541024092a3bdb1c337
yanis.mammar <yanis.mammar@s3ns.io>
2024-09-18 23:48:21 +0200

    add home-manager

--- default.nix
+++ default.nix
@@ -1,13 +1,19 @@
 {
   sources ? import ./npins,
   pkgs ? import sources.nixpkgs { },
+  home-manager ? import sources.home-manager {}
 }:
 
 rec {
   lib = { };
 
   systems = {
-    hpEliteBook = import ./hosts/hp-elite-book.nix;
+    hpEliteBook = { config, ... }: {
+      imports = [
+        home-manager.nixos
+        (import ./hosts/hp-elite-book.nix)
+      ];
+    };
   };
 
   nixosConfigurations = builtins.mapAttrs (name: config: pkgs.nixos [ config ]) systems;
