30ea83b5670340d1fd7a2126c3e439de77d0e00b
yanis.mammar <yanis.mammar@s3ns.io>
2024-09-19 00:08:46 +0200

    npins: switch to nixos-unstable

--- default.nix
+++ default.nix
@@ -1,6 +1,6 @@
 {
   sources ? import ./npins,
-  pkgs ? import sources.nixpkgs { },
+  pkgs ? import sources.nixos-unstable { },
   home-manager ? import sources.home-manager {}
 }:
 
@@ -9,6 +9,7 @@ rec {
 
   systems = {
     hpEliteBook = { config, ... }: {
+      _module.args = { pkgs = pkgs.lib.mkForce pkgs; };
       imports = [
         home-manager.nixos
         (import ./hosts/hp-elite-book.nix)
